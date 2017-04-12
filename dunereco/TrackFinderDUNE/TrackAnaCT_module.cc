a04462d1226d21181529c4fb5d56e40595b35c3a
Lynn Garren <garren@fnal.gov>
2017-04-12 10:29:37 -0500

    constructor FindMany will take an initializer list of ptr, not a single ptr

--- dunereco/TrackFinderDUNE/TrackAnaCT_module.cc
+++ dunereco/TrackFinderDUNE/TrackAnaCT_module.cc
@@ -1317,7 +1317,7 @@ namespace trkf {
 	  const art::Ptr<recob::Track> ptrack(*(it++));
 	  //	  const recob::Track& track = *ptrack;
 	  auto pcoll { ptrack };
-	  art::FindManyP<recob::SpacePoint> fs( pcoll, evt, fTrkSpptAssocModuleLabel);
+	  art::FindManyP<recob::SpacePoint> fs( {pcoll}, evt, fTrkSpptAssocModuleLabel);
 	  // From gdb> ptype fs, the vector of Ptr<SpacePoint>s it appears is grabbed after fs.at(0)
 	  bool assns(true);
 	  try {
