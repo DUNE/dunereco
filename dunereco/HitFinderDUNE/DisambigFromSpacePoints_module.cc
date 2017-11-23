8fe9c3917f1aa555cec48357667260cf224d755f
Robert Sulej <rsulej@dunebuild01.fnal.gov>
2017-11-23 09:27:19 -0600

    add options to drop unresolved hits, or put them on the first or all allowed wires, use (or not) neighboring hits

--- dunereco/HitFinderDUNE/DisambigFromSpacePoints_module.cc
+++ dunereco/HitFinderDUNE/DisambigFromSpacePoints_module.cc
@@ -43,14 +43,16 @@ namespace dune {
 
   class DisambigFromSpacePoints : public art::EDProducer {
   public:
-	struct Config {
-		using Name = fhicl::Name;
-		using Comment = fhicl::Comment;
+    struct Config {
+        using Name = fhicl::Name;
+        using Comment = fhicl::Comment;
 
-		fhicl::Atom<art::InputTag> HitModuleLabel { Name("HitModuleLabel"), Comment("HitPointSolver label.") };
-		fhicl::Atom<art::InputTag> SpModuleLabel { Name("SpModuleLabel"), Comment("SpacePointSolver label.") };
+        fhicl::Atom<art::InputTag> HitModuleLabel { Name("HitModuleLabel"), Comment("HitPointSolver label.") };
+        fhicl::Atom<art::InputTag> SpModuleLabel { Name("SpModuleLabel"), Comment("SpacePointSolver label.") };
         fhicl::Sequence<size_t> ExcludeTPCs { Name("ExcludeTPCs"), Comment("TPC inndexes where hits are not allowed.") };
+        fhicl::Atom<bool> UseNeighbors { Name("UseNeighbors"), Comment("Use neighboring hits to complete hits unresolved with spacepoints.") };
         fhicl::Atom<float> MaxDistance { Name("MaxDistance"), Comment("Distance [cm] used to complete hits unresolved with spacepoints.") };
+        fhicl::Atom<std::string> MoveLeftovers { Name("MoveLeftovers"), Comment("Mode of dealing with undisambiguated hits.") };
     };
     using Parameters = art::EDProducer::Table<Config>;
 
@@ -72,20 +74,38 @@ namespace dune {
             std::unordered_map< size_t, geo::WireID > & assignments,
             const std::vector< art::Ptr<recob::Hit> > & eventHits,
             cryo_tpc_plane_keymap & indHits,
+            std::vector<size_t> & unassigned,
+            size_t nNeighbors
+            ) const;
+
+    void assignFirstAllowedWire(
+            std::unordered_map< size_t, geo::WireID > & assignments,
+            const std::vector< art::Ptr<recob::Hit> > & eventHits,
             const std::vector<size_t> & unassigned
             ) const;
+
+    void assignEveryAllowedWire(
+            std::unordered_map< size_t, std::vector<geo::WireID> > & assignments,
+            const std::vector< art::Ptr<recob::Hit> > & eventHits,
+            const std::vector<size_t> & unassigned
+            ) const;
+
 
     geo::GeometryCore const* fGeom;
     detinfo::DetectorProperties const* fDetProp;
 
+    const bool fUseNeighbors;
     const float fMaxDistance;
+    const std::string fMoveLeftovers;
     const std::vector< size_t > fExcludeTPCs;
     const art::InputTag fHitModuleLabel;
     const art::InputTag fSpModuleLabel;
   };
 
   DisambigFromSpacePoints::DisambigFromSpacePoints(DisambigFromSpacePoints::Parameters const& config) :
+    fUseNeighbors(config().UseNeighbors()),
     fMaxDistance(config().MaxDistance()),
+    fMoveLeftovers(config().MoveLeftovers()),
     fExcludeTPCs(config().ExcludeTPCs()),
     fHitModuleLabel(config().HitModuleLabel()),
     fSpModuleLabel(config().SpModuleLabel())
@@ -145,13 +165,21 @@ namespace dune {
 
     cryo_tpc_plane_keymap indHits;                        // induction hits resolved with spacepoints
     std::vector<size_t> unassignedHits;                   // hits to resolve by neighoring assignments
-    std::unordered_map< size_t, geo::WireID > hitToWire;  // final hit-wire assignments
+    std::unordered_map< size_t, geo::WireID > hitToWire;                 // final hit-wire assignments
+    std::unordered_map< size_t, std::vector<geo::WireID> > hitToNWires;  // final hit-many-wires assignments
 
     int n = runOnSpacePoints(eventHits, spFromHit, spToTPC, hitToWire, indHits, unassignedHits);
     std::cout << n << " hits undisambiguated by space points." << std::endl;
 
-    n = resolveUnassigned(hitToWire, eventHits, indHits, unassignedHits);
-    std::cout << n << " hits undisambiguated at all (left on the first allowed wire segment)." << std::endl;
+    if (fUseNeighbors)
+    {
+        n = resolveUnassigned(hitToWire, eventHits, indHits, unassignedHits, 2);
+        std::cout << n << " hits undisambiguated by neighborhood." << std::endl;
+    }
+
+    if (fMoveLeftovers == "repeat")     { assignEveryAllowedWire(hitToNWires, eventHits, unassignedHits);      }
+    else if (fMoveLeftovers == "first") { assignFirstAllowedWire(hitToWire, eventHits, unassignedHits);        }
+    else                                { std::cout << "Remaining undisambiguated hits dropped." << std::endl; }
 
     for (auto const & hw : hitToWire)
     {
@@ -164,6 +192,20 @@ namespace dune {
         art::Ptr<raw::RawDigit> rawdigits = channelHitRawDigits.at(key);
 
         hcol.emplace_back(new_hit.move(), wire, rawdigits);
+    }
+
+    for (auto const & hws : hitToNWires)
+    {
+        size_t key = hws.first;
+        for (auto const & wid : hws.second)
+        {
+            recob::HitCreator new_hit(*(eventHits[key]), wid);
+
+            art::Ptr<recob::Wire> wire = channelHitWires.at(key);
+            art::Ptr<raw::RawDigit> rawdigits = channelHitRawDigits.at(key);
+
+            hcol.emplace_back(new_hit.move(), wire, rawdigits);
+        }
     }
 
     // put the hit collection and associations into the event
@@ -256,7 +298,8 @@ namespace dune {
     std::unordered_map< size_t, geo::WireID > & assignments,
     const std::vector< art::Ptr<recob::Hit> > & eventHits,
     cryo_tpc_plane_keymap & allIndHits,
-    const std::vector<size_t> & unassigned
+    std::vector<size_t> & unassigned,
+    size_t nNeighbors
     ) const
   {
     int nLeftInPlace = 0;
@@ -276,9 +319,8 @@ namespace dune {
         const float ddMax = dwMax * fGeom->TPC(0, 0).Plane(plane).WirePitch() / std::fabs(fDetProp->GetXTicksCoefficient(0, 0));
         const float maxDValue = 1000;
 
-        bool found = false;
-        float bestScore = maxDValue;
-        geo::WireID bestId = id;
+        float bestScore = 0;
+        geo::WireID bestId;
         for (size_t w = 0; w < cwids.size(); ++w)
         {
             const size_t tpc = cwids[w].TPC;
@@ -286,23 +328,23 @@ namespace dune {
 
             bool allowed = true;
             for (auto t : fExcludeTPCs) { if (t == tpc) { allowed = false; break; } }
-            if (!allowed)
-            {
-                if (w + 1 < cwids.size()) { bestId = cwids[w + 1]; }
-                continue;
-            }
+            if (!allowed) { continue; }
 
             const float wirePitch = fGeom->TPC(tpc, cryo).Plane(plane).WirePitch();
             const float driftPitch = std::fabs(fDetProp->GetXTicksCoefficient(tpc, cryo));
         
-            std::vector<float> distBuff(4, maxDValue); // distance of n closest hits
+            std::vector<float> distBuff(nNeighbors, maxDValue); // distance to n closest hits
             const auto & keys = allIndHits[cryo][tpc][plane];
             for (const size_t keyInd : keys)
             {
                 const auto & hitInd = eventHits[keyInd];
 
                 auto search = assignments.find(keyInd);        // find resolved wire id
-                if (search == assignments.end()) { continue; } // should be there, add warning
+                if (search == assignments.end())
+                {
+                    std::cout << "**** Did not find resolved wire id." << std::endl;
+                    continue;
+                }
 
                 float dWire = std::abs(hitWire - search->second.Wire);
                 float dDrift = std::fabs(hitDrift - hitInd->PeakTime());
@@ -325,27 +367,79 @@ namespace dune {
             size_t nhits = 0;
             for (size_t i = 0; i < distBuff.size(); ++i )
             {
-                if (distBuff[i] < maxDValue) { score += distBuff[i]; ++nhits; }
+                if (distBuff[i] < maxDValue) { score += 1./(1. + distBuff[i]); ++nhits; }
             }
             if (nhits > 0)
             {
-                score /= nhits;
-                if (score < bestScore)
+                if (score > bestScore)
                 {
                     bestScore = score;
                     bestId = cwids[w];
-                    found = true;
                 }
             }
         }
-        //std::cout << "plane:" << plane << ", best score: " << bestScore << std::endl;
-        result[key] = bestId;
-        if (!found) { ++nLeftInPlace; }
+        if (bestId.isValid) { result[key] = bestId; }
+        else { ++nLeftInPlace; }
+    }
+
+    // remove from list of unassigned hits
+    for (const auto & entry : result)
+    {
+        unassigned.erase(std::remove(unassigned.begin(), unassigned.end(), entry.first), unassigned.end());
     }
 
     //assignments.merge(result); // use this with C++ 17
     assignments.insert(result.begin(), result.end());
     return nLeftInPlace;
+  }
+
+  void DisambigFromSpacePoints::assignFirstAllowedWire(
+    std::unordered_map< size_t, geo::WireID > & assignments,
+    const std::vector< art::Ptr<recob::Hit> > & eventHits,
+    const std::vector<size_t> & unassigned
+    ) const
+  {
+    for (const size_t key : unassigned)
+    {
+        const auto & hit = eventHits[key];
+        std::vector<geo::WireID> cwids = fGeom->ChannelToWire(hit->Channel());
+        if (cwids.empty()) { continue; } // add warning here
+
+        geo::WireID bestId;
+        for (size_t w = 0; w < cwids.size(); ++w)
+        {
+            const size_t tpc = cwids[w].TPC;
+
+            bool allowed = true;
+            for (auto t : fExcludeTPCs) { if (t == tpc) { allowed = false; break; } }
+            if (allowed) { bestId = cwids[w]; break; }
+        }
+        if (bestId.isValid) { assignments[key] = bestId; }
+        else { std::cout << "None of wires is allowed for this hit???"; }
+    }
+  }
+
+  void DisambigFromSpacePoints::assignEveryAllowedWire(
+    std::unordered_map< size_t, std::vector<geo::WireID> > & assignments,
+    const std::vector< art::Ptr<recob::Hit> > & eventHits,
+    const std::vector<size_t> & unassigned
+    ) const
+  {
+    for (const size_t key : unassigned)
+    {
+        const auto & hit = eventHits[key];
+        std::vector<geo::WireID> cwids = fGeom->ChannelToWire(hit->Channel());
+        if (cwids.empty()) { continue; } // add warning here
+
+        for (size_t w = 0; w < cwids.size(); ++w)
+        {
+            const size_t tpc = cwids[w].TPC;
+
+            bool allowed = true;
+            for (auto t : fExcludeTPCs) { if (t == tpc) { allowed = false; break; } }
+            if (allowed) { assignments[key].push_back(cwids[w]); }
+        }
+    }
   }
 
   DEFINE_ART_MODULE(DisambigFromSpacePoints)
