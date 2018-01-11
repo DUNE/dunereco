f5220fd94a22d8295352fea6e9285ff417ee1ced
Tingjun Yang <tjyang@fnal.gov>
2018-01-11 14:01:52 -0600

    Using GenVector instead of TLorentzVector.

--- dunereco/FDSensOpt/FDSensOptData/EnergyRecoOutput.h
+++ dunereco/FDSensOpt/FDSensOptData/EnergyRecoOutput.h
@@ -1,21 +1,24 @@
 #ifndef FDSENSOPT_ENERGYRECOOUTPUT_H
 #define FDSENSOPT_ENERGYRECOOUTPUT_H
 
-#include "TLorentzVector.h"
+#include "Math/GenVector/PxPyPzE4D.h" 
+#include "Math/GenVector/LorentzVector.h" 
+
+using Position4_t = ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double>>;
 
 namespace dune
 {
   class EnergyRecoOutput
   {
   public:
-    
+
     int recoMethodUsed;         // 1 = longest reco track + hadronic, 2 = reco shower with highest charge + hadronic, 3 = all hit charges, -1 = not set 
 //    double nuEnergy;
 //    double lepEnergy;
 //    double hadEnergy;
-    TLorentzVector fNuLorentzVector;
-    TLorentzVector fLepLorentzVector;
-    TLorentzVector fHadLorentzVector;
+    Position4_t fNuLorentzVector;
+    Position4_t fLepLorentzVector;
+    Position4_t fHadLorentzVector;
     int longestTrackContained;  // 1 = contained, 0 = exiting, -1 = not set
     int trackMomMethod;         // 1 = range, 0 = MCS, -1 = not set
   };
