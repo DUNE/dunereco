19d8b275653948916e29f8b0b01f2665442384eb
Kyle Knoepfel <knoepfel@fnal.gov>
2018-10-23 16:02:06 -0500

    Partial migration to art 3.

--- dunereco/HitFinderDUNE/DisambigFromSpacePoints_module.cc
+++ dunereco/HitFinderDUNE/DisambigFromSpacePoints_module.cc
@@ -226,7 +226,7 @@ namespace dune {
     else if (fMoveLeftovers == "first") { assignFirstAllowedWire(hitToWire, eventHits, unassignedHits);        }
     else                { mf::LogInfo("DisambigFromSpacePoints") << "Remaining undisambiguated hits dropped."; }
 
-    auto const hitPtrMaker = art::PtrMaker<recob::Hit>(evt, *this);
+    auto const hitPtrMaker = art::PtrMaker<recob::Hit>(evt);
 
     for (auto const & hw : hitToWire)
     {
@@ -512,4 +512,3 @@ namespace dune {
 
 } // end of dune namespace
 #endif 
-
