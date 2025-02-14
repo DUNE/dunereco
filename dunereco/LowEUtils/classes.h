5e20c31a1e4936fbd08072b4e4da4936a5375dcd
mantheys <sergio.manthey@ciemat.es>
2025-02-14 13:39:41 +0100

    INCLUDE COMPILATION DEPENDENCIES

---
+++ dunereco/LowEUtils/classes.h
@@ -0,0 +1,10 @@
+#include "larcore/Geometry/WireReadout.h"
+#include "larreco/RecoAlg/Cluster3DAlgs/Cluster3D.h"
+
+#include "canvas/Persistency/Common/Assns.h"
+#include "canvas/Persistency/Common/Wrapper.h"
+
+#include <span>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <vector>
