9e214638483144b2a587a101333363d5f4e46051
mantheys <sergio.manthey@ciemat.es>
2025-02-24 19:50:01 +0100

    ADD NECESSARY CLASS DEFINITIONS

--- dunereco/LowEUtils/classes.h
+++ dunereco/LowEUtils/classes.h
@@ -1,10 +1,7 @@
-#include "larcore/Geometry/WireReadout.h"
-#include "larreco/RecoAlg/Cluster3DAlgs/Cluster3D.h"
-
 #include "canvas/Persistency/Common/Assns.h"
 #include "canvas/Persistency/Common/Wrapper.h"
 
-#include <span>
-#include <Eigen/Core>
-#include <Eigen/Dense>
+#include "lardataobj/RecoBase/Cluster.h"
+#include "dunereco/LowEUtils/LowECluster.h"
+
 #include <vector>
