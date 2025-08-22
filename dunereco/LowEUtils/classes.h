f6af0e7b1a6a4b6bb16fac9009a5bb0381a2d25d
mantheys <sergio.manthey@ciemat.es>
2025-08-22 09:10:40 -0500

    ADD CONFIG FOR LOWEEVENT ASSOCIATIONS

--- dunereco/LowEUtils/classes.h
+++ dunereco/LowEUtils/classes.h
@@ -1,7 +1,9 @@
 #include "canvas/Persistency/Common/Assns.h"
 #include "canvas/Persistency/Common/Wrapper.h"
 
+#include "lardataobj/RecoBase/Hit.h"
 #include "lardataobj/RecoBase/Cluster.h"
+#include "lardataobj/RecoBase/OpFlash.h"
 #include "dunereco/LowEUtils/LowECluster.h"
 #include "dunereco/LowEUtils/LowEEvent.h"
 
